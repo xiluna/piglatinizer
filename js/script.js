a72d0edfe87b06b02b09e8eaf872c7c6347a88d5
xiluna <xilaluna2@gmail.com>
2019-11-15 03:25:53 +0000

    The Piggy has been latinized

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 $( "document" ).ready(function() {
 $("#button").click(function() {
-    var text = $(".input").val();
-    var semiPigLatin = text.split()
-
-    $(".output").append(text + "ay");
+    let pigLatin = $(".input").val();
+    console.log(pigLatin, 'piglatin');
+    let vowel = pigLatin.match(/[aeiou]/);
+    console.log(vowel, 'vowel')
+    let vowelIndex = pigLatin.indexOf(vowel);
+    console.log(vowelIndex, 'vindex');
+    
+    if (vowelIndex > 0) {
+        const latinized = pigLatin.slice(vowelIndex) + pigLatin.slice(0, vowelIndex) + 'ay';
+        $('.output').text(latinized);
+    } else {
+        $('.output').text(pigLatin + 'way');
+    }
 });
 });
